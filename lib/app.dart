4920e3e62e8b5faff0475ecf1ebd3c7f636f0ac3
chintan <sharmachintan585@gmail.com>
2026-05-12 17:09:12 +0530

    fix: Definitively resolve ChatInput syntax error with full structural refresh

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
-import 'providers/chat_provider.dart';
 import 'providers/theme_provider.dart';
 import 'screens/chat_screen.dart';
 
