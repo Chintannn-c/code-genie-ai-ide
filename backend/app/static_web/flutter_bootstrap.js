e4aed4dc5e5e9d313c12e459a9b42fcfc4b1c936
chintan <sharmachintan585@gmail.com>
2026-05-12 17:34:14 +0530

    deploy: Update Flutter Web production build (Version 2.1)

--- backend/app/static_web/flutter_bootstrap.js
+++ backend/app/static_web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "559841131" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2569330023" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
